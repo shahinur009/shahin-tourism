837e518d577e225f5e84a2d3e50259834874ea08
shahin009 <shaheenmis170879@gmail.com>
2024-04-30 14:52:08 +0600

    all work done

--- src/firebase/frebas.config.js
+++ src/firebase/frebas.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBK0nDESAQjqeDdFydFiBSpCaMRsJevT3k",
-  authDomain: "shahin-tourism.firebaseapp.com",
-  projectId: "shahin-tourism",
-  storageBucket: "shahin-tourism.appspot.com",
-  messagingSenderId: "350722574935",
-  appId: "1:350722574935:web:97765c058b608ce7267112"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase

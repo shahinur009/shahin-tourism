b75b3efdb840ae54590517fb56d468b44426c165
shahin009 <shaheenmis170879@gmail.com>
2024-04-30 15:08:13 +0600

    ignore file done

--- src/Route/shared/PrivateRoute.jsx
+++ src/Route/shared/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import useAuth from "../../Provider/useAuth";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth() || {};
     const location = useLocation()
+    const navigate = useNavigate();
 
     if(loading){
         return <span className="loading loading-dots loading-lg items-center"></span>
